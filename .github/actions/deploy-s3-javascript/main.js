aa6e901db8bbd0a644eea41bc6c107ed94e5596b
Carrie <chanscom7@gmail.com>
2023-07-16 00:49:25 -0400

    add inputs for mock deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,16 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3uri = `s3://${bucket}`;
+    //exec.exec(`aws s3 sync ${distFolder} ${s3uri} --region ${bucketRegion}`);
+
     core.notice('This will be written to the GHA workflow log');
+    core.notice(`got inputs: ${bucket} ${bucketRegion} ${distFolder}`);
 }
 
 run();
